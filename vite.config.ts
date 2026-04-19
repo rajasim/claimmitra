2a64d66e3498533029867ee73a900166e2c5b406
Rajasim <Rajmahat@13gmail.com>
2026-04-19 14:57:09 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
+// @ts-ignore - Tell TS to ignore the property error
 export default defineConfig({
   server: {
     preset: "vercel",
